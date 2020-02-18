b3cb4372ad86a67c7375deba75ddcbf3a9b722f3
Vishal Brahmbhatt <vishal.milkeyway@gmail.com>
2020-02-18 13:15:34 +0530

    hhhff

--- kiosk/API/urls.py
+++ kiosk/API/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from .views import Kiosk
+from .views import Kiosk,ModelAPIView
 
 router = routers.DefaultRouter()
 router.register(r'Kiosk', Kiosk)
+router.register(r'Model', ModelAPIView)
 
 urlpatterns = []
 urlpatterns += router.urls
--- kiosk/UI/views.py
+++ kiosk/UI/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from ..API.views import *
+from API.views import *
 
 
 # Create your views here.
